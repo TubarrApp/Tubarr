988b24af4bc0eb7e54ce9acf8ac1142f2c4f7790
calumjones311 <calumjones@outlook.com>
2025-10-20 08:09:32 +0100

    Added validation for key, val string entries into database queries

--- internal/validation/db.go
+++ internal/validation/db.go
@@ -6,21 +6,21 @@ import (
 	"tubarr/internal/domain/consts"
 )
 
-// ValidateColumnKey ensures that the provided column 'key' is valid.
-func ValidateColumnKey(key string) error {
-	if key == "" {
-		return errors.New("key must not be empty")
-	}
-	if !consts.ValidDBColumns[key] {
-		return fmt.Errorf("invalid database column key: %q", key)
-	}
-	return nil
-}
+// // ValidateColumnKey ensures that the provided column 'key' is valid.
+// func ValidateColumnKey(key string) error {
+// 	if key == "" {
+// 		return errors.New("key cannot be empty")
+// 	}
+// 	if !consts.ValidDBColumns[key] {
+// 		return fmt.Errorf("invalid database column key: %q", key)
+// 	}
+// 	return nil
+// }
 
 // ValidateColumnKeyVal ensures that the provided column 'key' with value 'val' is valid.
 func ValidateColumnKeyVal(key, val string) error {
 	if key == "" || val == "" {
-		return errors.New("key and value must not be empty")
+		return errors.New("key and value cannot be empty")
 	}
 	if !consts.ValidDBColumns[key] {
 		return fmt.Errorf("invalid database column key: %q", key)
